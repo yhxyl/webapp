1b950be27d4425d8d071d7478a3838a6ba8c0271
yhxyl <641997917@qq.com>
2022-08-07 21:16:44 +0800

    最初环境

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>hello world</div>
+</template>
+
+<script lang="ts">
+    import { Vue, Options } from "vue-class-component";
+    @Options({
+        created(){
+            const a:string = '123'
+            console.log(a)
+        }
+    })
+    export default class App extends Vue {
+        
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');	
+module.exports = {
+    entry: './src/main.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+		clean:true
+    },
+    resolve:{
+        alias: {
+            '@': path.join(__dirname, 'src')
+        },
+	    extensions: ['vue', '.js', '.ts']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    {loader:'style-loader'},
+                    {loader:'css-loader'}
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: 'asset/resource'
+            },
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+				options:{
+					appendTsSuffixTo:[/\.vue$/]
+				}
+			}
+        ]
+    },
+    devtool: 'inline-source-map',
+    plugins:[
+        new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			filename:'index.html',
+			template:'./public/index.html'
+		})
+    ],
+	devServer: {
+		static:'./dist'
+	}
+};
